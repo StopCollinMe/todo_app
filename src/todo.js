04eeba6df061c2280952310c719316300ea69d24
Collin Uemura <collinuemura@gmail.com>
2023-05-19 22:13:06 -0700

    Added more features

--- src/todo.js
+++ src/todo.js
@@ -2,31 +2,59 @@ import _ from 'lodash';
 import './styles.css';
 import 'date-fns';
 import '@mdi/font/css/materialdesignicons.css';
+import './projectCreator';
+import { TodoList } from './projectCreator';
 
 
 //Automates selecting element, adding an event listener, and adding functionality
 //Was spicy. Proud of myself for this one
-const addButtonClick = (ref, type, func) => {
+const addButtonClick = (ref, type, ...funcs) => {
     if(type == 'class'){
         const button = document.querySelector(`.${ref}`);
-        button.addEventListener('click', func);
+        button.addEventListener('click', () =>{
+            funcs.forEach(func => func());
+        });
     }
     else if(type == 'id'){
         const button = document.querySelector(`#${ref}`);
-        button.addEventListener('click', func);
+        button.addEventListener('click', () =>{
+            funcs.forEach(func => func());
+        });
     }
 }
 
-const handleAlert = () => {
-    alert('Yay!');
+
+const grabDom = (element) =>{
+    const item = document.querySelector(`.${element}`);
+    return item;
 }
 
-const openMenu = () => {
-    //Add class
-    //Remove class on second round
+//SUCCESSFULLY MADE SOME DYNAMIC CODE!!!!!
+const toggleMenu = (select, className) =>{
+    const element = document.querySelector(`.${select}`);
+    element.classList.toggle(`${className}`);
 }
 
-addButtonClick('mdi-menu','class', () => openMenu());
+const newProject = () => {
+
+}
+
+
+//On click add new html file that shows a screen to make a new object from ToDoList
+//Also takes in Project Name and adds it to left side menu.
+//After Project Name is added a
+addButtonClick('mdi-plus','class', ()=> newProject());
+addButtonClick('mdi-menu','class',()=> toggleMenu('menu','hide'),()=> 
+toggleMenu('mdi-menu','tabbed'), ()=> 
+toggleMenu('display','toggleDisplay'));
+
+//Make this dynamic based off of input via webpage.
+//Add limitations
+//Add calender? Might be too ambitious
+
+const first = new TodoList('first', 'description','3/30', 'first', 'Yes', 'checklist');
+
+console.log(first);
 
 //Add projectAdder to menu
 //Add projectChild to project created by projectAdder
